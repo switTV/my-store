0af178b3517d6bcc65d0226364d078be56c29815
switt_tv <4galarzajuan2004@gmail.com>
2023-04-27 15:13:37 -0600

    commit 2

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-
-app.set("port", process.env.PORT || 3000)
+const PORT = 3000;
 
 app.get("/products", (req, res) => {
     res.json([{
-        name: "Heladera",
-        precio: 250,
+        type: "remera",
+        name: "Remera Trasher",
+        talle:"XL",
+        precio: 122,
+        img_producto: "https://imgs.search.brave.com/wtdeUV5-ofK4s8v76Io3FehhMZ3i8rKtQUs6EGLZb2U/rs:fit:1100:1100:1/g:ce/aHR0cHM6Ly93d3cu/Ymxvd2xhbmQuY29t/LmFyLzE2NzE3LXRo/aWNrYm94X2RlZmF1/bHQvcmVtZXJhLXRo/cmFzaGVyLWZsYW1l/LWxpZmVzdHlsZS1u/aW5vLmpwZw"
     },
     {
-        name: "Cocina",
-        precio: 300,
+        type: "pantalon",
+        name: "Pantalon Adidas",
+        talle: 12,
+        precio: 40,
+        img_producto: "https://imgs.search.brave.com/ak2hskGe_7ht4f7JOBQtNOMWSxfOi9z3iYSxEN_tMcQ/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9yZXMu/Y2xvdWRpbmFyeS5j/b20vZnV0Ym9sLWZh/Y3RvcnkvaW1hZ2Uv/dXBsb2FkL2NfcGFk/LGZfYXV0byxoXzIw/MDAsd18xNjAwL3Yx/L3Byb2R1Y3RzLzIy/NDg4OF8yLmpwZw"
     }
-
     ])
 })
 
@@ -25,6 +29,13 @@ app.get("/users", (req, res) => {
             offset
         })
     } else {
-        res.send("no hay nadadadad")
+        res.send("no se encuentra la info, fijate el link.")
+
     }
+})
+
+
+
+app.listen(PORT, () => {
+    console.log('Mi puerto es: ' + PORT);
 })
